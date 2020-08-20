2567c3a67d4733b87a7192707b0c2252872b1978
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-08-19 19:42:12 -0700

    fixed package.json

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const mongoose = require('mongoose');
 const server = require('./lib/server.js');
+
+const MONGODB_URI = 'mongodb://localhost:27017/products';
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(MONGODB_URI, mongooseOptions);
+
 
 server.start(3002);
