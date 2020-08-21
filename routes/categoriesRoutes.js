8a69aaaf9f59da6e58b072e05e51173a54e932fa
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-08-20 18:24:53 -0700

    created generic API route

--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -1,46 +1,46 @@
-'use strict';
+// 'use strict';
 
-const express = require('express');
-const categoriesRoutes = express.Router();
+// const express = require('express');
+// const categoriesRoutes = express.Router();
 
-const categories = require('../lib/models/categories/categories.collection.js');
+// const categories = require('../lib/models/categories/categories.collection.js');
 
-categoriesRoutes.post('/categories', postCategories);
-categoriesRoutes.post('/categories/:id', postCategories);
-categoriesRoutes.get('/categories', getCategories);
-categoriesRoutes.get('/categories/:id', getCategories);
-categoriesRoutes.put('/categories', putCategories);
-categoriesRoutes.put('/categories/:id', putCategories);
-categoriesRoutes.delete('/categories/:id', deleteCategories);
-
-
-function postCategories (req, res, next) {
-  categories.post()
-    .then(data => {
-      res.status(200).json(data);
-    });
-}
-
-function getCategories(req, res, next){
-  categories.get()
-    .then(data=>{
-      res.status(200).json(data);
-    });
-}
-
-function putCategories (req, res, next){
-  categories.put()
-    .then(data => {
-      res.status(200).json(data);
-    });
-}
-
-function deleteCategories (req, res, next){
-  categories.put()
-    .then(data => {
-      res.status(200).json(data);
-    });
-}
+// categoriesRoutes.post('/categories', postCategories);
+// categoriesRoutes.post('/categories/:id', postCategories);
+// categoriesRoutes.get('/categories', getCategories);
+// categoriesRoutes.get('/categories/:id', getCategories);
+// categoriesRoutes.put('/categories', putCategories);
+// categoriesRoutes.put('/categories/:id', putCategories);
+// categoriesRoutes.delete('/categories/:id', deleteCategories);
 
 
-module.exports = categoriesRoutes;
+// function postCategories (req, res, next) {
+//   categories.post()
+//     .then(data => {
+//       res.status(200).json(data);
+//     });
+// }
+
+// function getCategories(req, res, next){
+//   categories.get()
+//     .then(data=>{
+//       res.status(200).json(data);
+//     });
+// }
+
+// function putCategories (req, res, next){
+//   categories.put()
+//     .then(data => {
+//       res.status(200).json(data);
+//     });
+// }
+
+// function deleteCategories (req, res, next){
+//   categories.put()
+//     .then(data => {
+//       res.status(200).json(data);
+//     });
+// }
+
+
+// module.exports = categoriesRoutes;
